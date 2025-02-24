fa99f1af5fabaa510a5acfe9c2a71c95df462763
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-02-25 00:27:48 +0800

    makes interactive with user
    changing the code from yt just a bit

--- audiobook.py
+++ audiobook.py
@@ -3,8 +3,8 @@ import PyPDF2
 book = open('461.pdf', 'rb')
 pdfReader = PyPDF2.PdfReader(book)
 pages = len(pdfReader.pages)
-print(pages)
-page = pdfReader.pages[3]
+page_to_read = int(input("What page would you like to listen to? (0-66) \n "))
+page = pdfReader.pages[page_to_read]
 text = page.extract_text()
 speaker = pyttsx3.init()
 speaker.say(text)
