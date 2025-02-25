11fcf5c3960edf699d7f4a14fd3fcb6bb107f34e
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-02-25 10:15:17 +0800

    Change the pdf file into Harry Potter series
    Changed for more interest by the user

--- audiobook.py
+++ audiobook.py
@@ -1,12 +1,14 @@
 import pyttsx3
 import PyPDF2
-book = open('461.pdf', 'rb')
+book = open('harrypotter.pdf', 'rb')
 pdfReader = PyPDF2.PdfReader(book)
 pages = len(pdfReader.pages)
+print("This is the Complete Harry Potter Collection")
+print("")
 page_to_read = int(input("What page would you like to listen to? (0-66) \n "))
 page = pdfReader.pages[page_to_read]
 text = page.extract_text()
 speaker = pyttsx3.init()
 speaker.say(text)
 speaker.runAndWait()
-print("donee")    
+print("Done!")    
